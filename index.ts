28ccf9e4557e1cc321b752ebe8e68242c88b7efe
Fullchee Zhang <fullchee@gmail.com>
2020-12-17 12:58:57 -0500

    Get the jwt working

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import cors from "cors";
 import { createAccount, deleteAccount } from "./src/psql/db-operations";
 import { getRefToken } from "./src/toronto-water/toronto-water";
+import { verify } from "jsonwebtoken";
 
 const PORT = process.env.PORT || 8000;
 
@@ -13,6 +14,28 @@ app.use(cors());
 app.get("/", (req, res) => res.send("Express + TypeScript Server"));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
+
+app.get("/verify", (req, res) => {
+  try {
+    const decoded = verify(
+      req.query.jwt as string,
+      process.env.JWT_SECRET as string
+    );
+    // TODO: send a web page that will send a post request
+    res.status(200).send("Authenticated token!");
+  } catch (error) {
+    console.error("Invalid JWT");
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).send("Your activation link expired");
+    } else {
+      return res
+        .status(400)
+        .send(
+          "Something wasn't right with your link. Could you check if you changed the URL accidentally?"
+        );
+    }
+  }
 });
 
 app.post("/create-account", async (req, res) => {
